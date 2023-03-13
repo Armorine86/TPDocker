1e423be370e5a5a4e38bb2c0763dadd60263775b
Maxime Mondello <mmondello@voila.app>
2023-03-13 19:58:06 -0400

    adding comments

--- requirements/site02/html/index.php
+++ requirements/site02/html/index.php
@@ -11,6 +11,8 @@
     $dbuser = 'root';
     $dbpass = 'mypassword';
     $dbhost = 'location';
+    
+    //* php sert de pont avec la DB mariaDB. À partir de la fonction mysql_connect on peut interfacer et manipuler la DB
     $connect = mysql_connect($dbhost, $dbuser, $dbpass) or die("Unable to connect to '$dbhost'");
     echo "Connexion a MariaDB Reussi"
 ?>
